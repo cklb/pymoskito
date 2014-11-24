2c2e5486ae3dee4ce1b704daaa1762f4037a12c0
Stefan Ecklebe <stefan.ecklebe@mailbox.tu-dresden.de>
2014-11-24 22:46:22 +0100

    work has begun

--- logging.py
+++ logging.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import time
-
-import matplotlib.pyplot as plt 
-plt.ion()
-from settings import *
+import sys, time
+from PyQt4.QtCore import QObject, pyqtSignal
 
 #--------------------------------------------------------------------- 
 # data logging helper
 #--------------------------------------------------------------------- 
-
-class SuperLogger:
+class Logger(QObject):
     """ Main Data Logger
     """
 
@@ -38,8 +34,6 @@ class SuperLogger:
             self.timestep += 1
 
     def update(self):
-        #update all subscriptions that got new data
-        updated_callbacks = []
         for item in self.subscribers:
             #build paket
             paket = {}
@@ -62,136 +56,4 @@ class SuperLogger:
         with open(self.filename, 'w+') as f:
             f.write(repr(self.data))
     
-class GraphLogger(SuperLogger):
-    ''' 
-    Provides y(x) plotting for data
-    '''
 
-
-    def __init__(self, name='Untitled', dimensions=1, yonly=False, x_range=[0, 10]):
-        Logger.__init__(self, dimensions)
-
-        self.yonly = yonly
-        #create a plot for every dimension
-        self.figure, self.axes = plt.subplots(self.dim, sharex=True)
-        self.lines = []
-        for i in range(self.dim):
-            if yonly:
-                self.lines.append(self.axes.plot([], [], 'b-')[0])
-                self.axes.set_autoscalex_on(True)
-                self.axes.set_autoscaley_on(True)
-                self.axes.grid()
-            else:
-                self.lines.append(self.axes[i].plot([], [], 'b-')[0])
-                self.axes[i].set_autoscaley_on(True)
-                self.axes[i].set_xlim(x_range)
-                self.axes[i].grid()
-
-    def log(self, x, y):
-        '''
-        expects 1 value for x-axis and a list of values for y-axis
-        '''
-        if x is not None:
-            self.data['x'].append(x)
-        else:
-            if len(self.data['x']) == 0:
-                self.data['x'].append(0)
-            else:
-                self.data['x'].append(self.data['x'][-1] + 1)
-
-        if len(y) != self.dim:
-            print 'Error: Y-Data corrupt:', len(y), '!=', self.dim
-            return
-
-        for index, val in enumerate(y):
-            self.data['y'][index].append(val)
-        
-        self.takeAction()
-
-    def takeAction(self):
-        ''' 
-        Implements the actual plotting
-        '''
-
-        for index, line in enumerate(self.lines):
-            line.set_xdata(self.data['x'])
-            line.set_ydata(self.data['y'][index])
-            if self.yonly:
-                self.axes.relim()
-                self.axes.autoscale_view()
-            else:
-                self.axes[index].relim()
-                self.axes[index].autoscale_view()
-
-        self.figure.canvas.draw()
-        self.figure.canvas.flush_events()
-        return
-
-class QtGraphLogger(SuperLogger):
-    ''' 
-    Provides y(x) plotting for data
-    '''
-
-
-    def __init__(self, name='Untitled', dimensions=1, yonly=False, x_range=[0, 10]):
-        Logger.__init__(self, dimensions)
-
-
-
-        
-
-        self.yonly = yonly
-        #create a plot for every dimension
-        self.figure, self.axes = plt.subplots(self.dim, sharex=True)
-        self.lines = []
-        for i in range(self.dim):
-            if yonly:
-                self.lines.append(self.axes.plot([], [], 'b-')[0])
-                self.axes.set_autoscalex_on(True)
-                self.axes.set_autoscaley_on(True)
-                self.axes.grid()
-            else:
-                self.lines.append(self.axes[i].plot([], [], 'b-')[0])
-                self.axes[i].set_autoscaley_on(True)
-                self.axes[i].set_xlim(x_range)
-                self.axes[i].grid()
-
-    def log(self, x, y):
-        '''
-        expects 1 value for x-axis and a list of values for y-axis
-        '''
-        if x is not None:
-            self.data['x'].append(x)
-        else:
-            if len(self.data['x']) == 0:
-                self.data['x'].append(0)
-            else:
-                self.data['x'].append(self.data['x'][-1] + 1)
-
-        if len(y) != self.dim:
-            print 'Error: Y-Data corrupt:', len(y), '!=', self.dim
-            return
-
-        for index, val in enumerate(y):
-            self.data['y'][index].append(val)
-        
-        self.takeAction()
-
-    def takeAction(self):
-        ''' 
-        Implements the actual plotting
-        '''
-
-        for index, line in enumerate(self.lines):
-            line.set_xdata(self.data['x'])
-            line.set_ydata(self.data['y'][index])
-            if self.yonly:
-                self.axes.relim()
-                self.axes.autoscale_view()
-            else:
-                self.axes[index].relim()
-                self.axes[index].autoscale_view()
-
-        self.figure.canvas.draw()
-        self.figure.canvas.flush_events()
-        return
