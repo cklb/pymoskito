a84746cd2608dede68cbb5a5137cbaaa63c7af62
cle92 <clemens.hruschka@googlemail.com>
2014-12-18 11:16:30 +0100

    beseitige fehler merge

--- observer.py
+++ observer.py
@@ -6,7 +6,6 @@ from scipy.integrate import ode
 import settings as st
 import tools as tools
 
-import tools as tools
 from sim_core import SimulationModule
 from linearization import Linearization
 
