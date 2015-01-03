e83b2fac54b128646c868b2ed8b1d7153f027760
cle92 <clemens.hruschka@googlemail.com>
2015-01-03 16:52:03 +0100

    u als theta_dd maximalen Fehler in zwischen 40. und 60. Sekunde

--- postprocessing/hauserDiagramsMatPlot.py
+++ postprocessing/hauserDiagramsMatPlot.py
@@ -5,8 +5,8 @@ import os
 
 import matplotlib as mpl
 mpl.use("Qt4Agg")
-mpl.rcParams['text.usetex']=True
-mpl.rcParams['text.latex.unicode']=True
+#mpl.rcParams['text.usetex']=True
+#mpl.rcParams['text.latex.unicode']=True
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 from matplotlib.figure import Figure
@@ -31,6 +31,7 @@ class hauserDiagramsMatPlot(PostProcessingModule):
         vSubt = np.vectorize(self.subt)
         vMul = np.vectorize(self.mul)
         vAdd = np.vectorize(self.add)
+        vDiv = np.vectorize(self.div)
           
         #calculate datasets
         t = data['results']['simTime']
@@ -40,8 +41,14 @@ class hauserDiagramsMatPlot(PostProcessingModule):
             y.append(data['results']['model_output.'+str(i)]  )
             
         eps = vSubt(yd[0], y[0])
-
-        u = data['results']['controller_output.0']
+        
+        # controlleroutput is a torque
+        tau = data['results']['controller_output.0']
+        # u in the neglected nonlinearity is theta_dd
+        u = vDiv(vSubt(vSubt(tau,\
+                             np.dot(np.dot(np.dot(2*st.M, y[0]), y[1]), y[3])),\
+                             np.dot(st.M*st.G, np.cos(y[2]))),\
+                 vAdd(vAdd(np.dot(st.M, np.power(y[0], 2)), st.J), st.Jb))
         
         # Parameter from Controller -> make modelling (estimate/meausre paramters)
         # and then neglect psi therm
@@ -72,7 +79,7 @@ class hauserDiagramsMatPlot(PostProcessingModule):
         axes1.plot(t, eps, c='k')
         axes1.set_xlim(left=0, right=t[-1])
         axes1.set_xlabel(r'$t /s$')
-        axes1.set_ylabel(r'$e$')
+        axes1.set_ylabel(r'$e /m$')
         
         axes2 = fig.add_subplot(2, 2, 2)
         #axes2.set_title(r'\textbf{Beam Angle}')
@@ -80,6 +87,7 @@ class hauserDiagramsMatPlot(PostProcessingModule):
         axes2.set_xlim(left=0, right=t[-1])
         axes2.set_xlabel(r'$t /s$')
         axes2.set_ylabel(r'$\theta$')
+            
         
         axes3 = fig.add_subplot(2, 2, 3)
         #axes3.set_title(r'\textbf{neglected nonlinearity}')
@@ -87,17 +95,34 @@ class hauserDiagramsMatPlot(PostProcessingModule):
         axes3.set_xlim(left=0, right=t[-1])
         axes3.set_xlabel(r'$t /s$')
         axes3.set_ylabel(r'$\psi$')
+        if data['modules']['controller']['type'] == 'FController' or \
+           data['modules']['controller']['type'] == 'JController':
+               axes3.set_ylabel(r'$\theta /\frac{m}{s^2}$')
+        if data['modules']['controller']['type'] == 'GController':
+            axes3.set_ylabel(r'$\theta /\frac{m}{s^3}$')
         
         axes4 = fig.add_subplot(2, 2, 4)
         #axes4.set_title(r'\textbf{Beam Torque}')
-        axes4.plot(t, u, c='k')
+        axes4.plot(t, tau, c='k')
         axes4.set_xlim(left=0, right=t[-1])
         axes4.set_xlabel(r'$t /s$')
-        axes4.set_ylabel(r'$\tau$')
+        axes4.set_ylabel(r'$\tau /Nm$')
         
-
+        # calculate epsilon_max
+        start = 40
+        end = 60
+        
+        for i in t:
+            if i >= start:
+                indStart = t.index(i)
+            if i >= end:
+                indEnd = t.index(i)
+                break
+        epsilon_max = max(eps[indStart:indEnd +1])
+        
+        
         #collect results
-        output = {'epsilon_max': 0}
+        output = {'epsilon_max': epsilon_max}
 
         #write results
         filePath = os.path.join(os.path.pardir, 'results', 'postprocessing', 'HauserDiagrams')
