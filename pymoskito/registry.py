a763c4efc7036de333217178fbb9bea87da68d5f
Stefan Ecklebe <stefan.ecklebe@tu-dresden.de>
2017-05-30 08:35:19 +0200

    Appended docs in registry

--- pymoskito/registry.py
+++ pymoskito/registry.py
@@ -13,23 +13,34 @@ _registry = {}
 
 def register_module(module_cls, module_type, cls, type_check=True):
     """
-    main hook to register a class in the pymoskito framework, if cls is not a subclass of module_type a TypeError
-    will be raised. If cls is already registered for module_type, ValueError will be raised.
+    Main hook to register a class (pymoskito module) in the pymoskito framework.
+    
+    Args:
+        module_cls (cls): Either :py:class:`pm.SimulationModule` or 
+            :py:class:`pm.ProcessingModule` 
+        module_type (cls): Type of the module, these are either the classes 
+            listed in `simulation_modules.py`, 
+            :py:class:`pm.PostProcessingModule` or 
+            :py:class:`pm.MetaProcessingModule` .
+        cls (cls): Class to be registered.
+        type_check (bool): Perform type checking (defaults to True)
+     
+    Raises:
+        TypeError: If `cls` is not a subclass of `module_type` .
+        ValueError: If `cls` is already registered for `module_type` .
 
-    :param module_type:
-    :param module_cls:
-    :param cls: class to be registered
-    :param type_check: perform type checking
     """
     if type_check:
         if not issubclass(cls, module_type):
-            raise TypeError("Module must match Type to be registered for! {0} <> {1}".format(cls, module_type))
+            raise TypeError("Module must match Type to be registered for "
+                            "{0} <> {1}".format(cls, module_type))
 
     cls_entry = _registry.get(module_cls, {})
     entry = cls_entry.get(module_type, [])
     increment = (cls, cls.__name__)
     if increment in entry:
-        raise ValueError("class {} already registered for module {}!".format(cls, module_cls))
+        raise ValueError("class {} already registered for module "
+                         "{}!".format(cls, module_cls))
 
     entry.append(increment)
 
@@ -42,10 +53,18 @@ def register_module(module_cls, module_type, cls, type_check=True):
 
 def get_registered_modules(module_cls, module_type):
     """
-    main hook to retrieve registered classes of a module type for a specific module class
-    :param module_cls:
-    :param module_type:
-    :return:
+    Return registered classes of a module type for a specific module class.
+    
+    Args:
+        module_cls (cls): Either :py:class:`pm.SimulationModule` or 
+            :py:class:`pm.ProcessingModule` 
+        module_type (cls): Type of the module, these are either the classes 
+            listed in `simulation_modules.py`, 
+            :py:class:`pm.PostProcessingModule` or
+            :py:class:`pm.MetaProcessingModule` .
+            
+    Returns:
+        list: List of (obj, string) tuples.
     """
     return _registry.get(module_cls, {}).get(module_type, [])
 
@@ -57,7 +76,8 @@ def get_module_class_by_name(module_cls, module_type, module_name):
     :param module_type:
     :param module_cls:
     """
-    return next((mod[0] for mod in get_registered_modules(module_cls, module_type)
+    return next((mod[0] for mod in get_registered_modules(module_cls,
+                                                          module_type)
                  if mod[1] == module_name), None)
 
 
@@ -72,8 +92,9 @@ def register_simulation_module(module_type, cls):
         raise TypeError("Only Simulation Modules can be registered!")
     if not issubclass(cls, module_type):
         # more complex inheritance allowed for mixers
-        if not (issubclass(module_type, SignalMixer) and issubclass(cls, SignalMixer)):
-            raise TypeError("Class must be instance of {0}!".format(module_type))
+        if not (issubclass(module_type, SignalMixer)
+                and issubclass(cls, SignalMixer)):
+            raise TypeError("Class must be instance of {0}".format(module_type))
 
     register_module(SimulationModule, module_type, cls, type_check=False)
 
@@ -88,6 +109,17 @@ def get_registered_simulation_modules(module_type):
 
 
 def get_simulation_module_class_by_name(module_type, module_name):
+    """
+    Return the class of a certain simulation module given its registered name.
+    
+    Args:
+        module_type (cls): Type of the module, 
+            see :py:func:`register_simulation_module` .
+        module_name (str): name of the module. 
+
+    Returns:
+
+    """
     return get_module_class_by_name(SimulationModule, module_type, module_name)
 
 
@@ -124,12 +156,14 @@ def register_visualizer(vis_cls):
     :param vis_cls: class to be registered
     """
     if not issubclass(vis_cls, Visualizer):
-        raise TypeError("Module must match type to be registered for! {0} <> {1}".format(vis_cls, Visualizer))
+        raise TypeError("Module must match type to be registered for! "
+                        "{0} <> {1}".format(vis_cls, Visualizer))
 
     cls_entry = _registry.get(Visualizer, [])
     increment = (vis_cls, vis_cls.__name__)
     if increment in cls_entry:
-        raise ValueError("class {0} already registered as visualizer!".format(vis_cls))
+        raise ValueError("class {0} already registered as visualizer!"
+                         "".format(vis_cls))
 
     cls_entry.append(increment)
     _registry[Visualizer] = cls_entry
