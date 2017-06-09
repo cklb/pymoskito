464ef65c6af430a9ee540ba67069f9df097feb3d
Stefan Ecklebe <stefan.ecklebe@tu-dresden.de>
2017-06-09 13:39:34 +0200

    Fixed issue in examples that snarled their adaption for own use cases

--- pymoskito/examples/ballbeam/__main__.py
+++ pymoskito/examples/ballbeam/__main__.py
@@ -1,17 +1,22 @@
 # -*- coding: utf-8 -*-
 import sys
 import os
+import importlib
 from PyQt5.QtWidgets import QApplication
 
 import pymoskito as pm
 
 if __name__ == '__main__':
-    parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    # This part lets us execute this example with:
+    #  $python -m pymoskito.examples.name
+    pkg_path = os.path.dirname(os.path.abspath(__file__))
+    pkg_name = pkg_path.split(os.path.sep)[-1]
+    parent_dir = os.path.dirname(pkg_path)
     sys.path.insert(0, parent_dir)
 
     if __package__ is None or __package__ == '':
-        import ballbeam
-        __package__ = "ballbeam"
+        importlib.import_module(pkg_name)
+        __package__ = pkg_name
 
     # create an Application instance (needed)
     app = QApplication([])
@@ -23,7 +28,7 @@ if __name__ == '__main__':
 
         # load default config
         prog.load_regimes_from_file(os.path.join(parent_dir,
-                                                 "ballbeam",
+                                                 pkg_name,
                                                  "default.sreg"))
         prog.apply_regime_by_name("test-nonlinear")
     else:
--- pymoskito/examples/car/__main__.py
+++ pymoskito/examples/car/__main__.py
@@ -1,21 +1,32 @@
 # -*- coding: utf-8 -*-
 import sys
 import os
+import importlib
 from PyQt5.QtWidgets import QApplication
 
 import pymoskito as pm
 
 if __name__ == '__main__':
-    parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    # This part lets us execute this example with:
+    #  $python -m pymoskito.examples.name
+    pkg_path = os.path.dirname(os.path.abspath(__file__))
+    pkg_name = pkg_path.split(os.path.sep)[-1]
+    parent_dir = os.path.dirname(pkg_path)
     sys.path.insert(0, parent_dir)
 
     if __package__ is None or __package__ == '':
-        import car
-        __package__ = "car"
+        importlib.import_module(pkg_name)
+        __package__ = pkg_name
 
+    # create an Application instance (needed)
     app = QApplication([])
+    prog = None
     sim = pm.Simulator()
-    sim.load_regimes_from_file(os.path.join(parent_dir, "car", "default.sreg"))
+
+    # load defaults
+    sim.load_regimes_from_file(os.path.join(parent_dir,
+                                            pkg_name,
+                                            "default.sreg"))
     sim.apply_regime_by_name("test")
     sim.show()
 
