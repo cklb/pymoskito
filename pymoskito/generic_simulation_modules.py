Added tests for ModelInputLimiter and fixed sign error

pymoskito/generic_simulation_modules.py
@@ -37,6 +37,8 @@ class LinearStateSpaceModel(Model):
         ("config file", None),
         ("initial state", None),
         ("initial output", None),
+        ("operational input offset", None),
+        ("operational output offset", None),
     ])
 
     def __init__(self, settings):
@@ -67,13 +69,32 @@ class LinearStateSpaceModel(Model):
             settings["initial state"] = \
                 np.linalg.pinv(self.ss.C) @ settings["initial output"]
 
+        if settings["operational input offset"] is None:
+            settings["operational input offset"] = np.zeros(
+                (self.ss.B.shape[1], ))
+        if len(settings["operational input offset"]) != self.ss.B.shape[1]:
+            raise ValueError("Provided input offset does not match input "
+                             "dimensions.")
+
+        if settings["operational output offset"] is None:
+            settings["operational output offset"] = np.zeros(
+                (self.ss.C.shape[0], ))
+        if len(settings["operational output offset"]) != self.ss.C.shape[0]:
+            raise ValueError("Length of provided output offset does not match "
+                             "output dimensions ({} != {}).".format(
+                len(settings["operational output offset"]),
+                self.ss.C.shape[0]
+            ))
+
         super().__init__(settings)
 
     def state_function(self, t, x, args):
-        return np.squeeze(self.ss.A @ x + self.ss.B @ args[0])
+        return np.squeeze(self.ss.A @ x + self.ss.B @ (
+            args[0] - self.settings["operational input offset"]))
 
     def calc_output(self, input_vector):
-        return self.ss.C @ input_vector
+        return (self.ss.C @ input_vector
+                + self.settings["operational output offset"])
 
 
 class ODEInt(Solver):
@@ -360,28 +381,34 @@ class AdditiveMixer(SignalMixer):
 
 class ModelInputLimiter(Limiter):
     """
-    ModelInputLimiter that limits the model input value
+    ModelInputLimiter that limits the model input values.
+
+    Settings:
+        `Limits`: (List of) list(s) that hold (min, max) pairs for the
+        corresponding input.
     """
 
-    public_settings = OrderedDict([("Limits", [0, 240])])
+    public_settings = OrderedDict([("Limits", [None, None])])
 
     def __init__(self, settings):
-        settings.update([("input signal", "ModelMixer")])
+        settings.update([("input_signal", "ModelMixer")])
         Limiter.__init__(self, settings)
+        self.limits = np.atleast_2d(settings["Limits"])
 
-    def _limit(self, value):
-        if value < self._settings["Limits"][0]:
-            # convert number into numpy (1,1) array
-            value = np.array(self._settings["Limits"][0])
-        if value > self._settings["Limits"][1]:
-            # convert number into numpy (1,1) array
-            value = np.array(self._settings["Limits"][1])
+    def _limit(self, values):
+        val = np.atleast_1d(values)
+        out = np.zeros_like(val)
+        for idx, v in enumerate(val):
+            lim = self.limits[idx]
+            if lim[0] is None and lim[1] is None:
+                out[idx] = v
+            else:
+                out[idx] = np.clip(v, *lim)
 
-        # TODO restructure to something like this:
-        # value = np.max(value, self._settings["Limits"][0])
-        # value = np.min(value, self._settings["Limits"][1])
-
-        return value
+        if len(values.shape) == 1:
+            return out.flatten()
+        else:
+            return out
 
 
 class DeadTimeSensor(Sensor):

pymoskito/simulation_interface.py
@@ -515,17 +515,17 @@ class SimulatorInteractor(QObject):
             m_data = self._get_result_by_name("Model")
             t_data = self._get_result_by_name("Trajectory")
             if len(t_data.shape) == 2:
-                c_error = t_data[:, 0] - m_data[..., 0]
+                c_error = m_data[..., 0] - t_data[:, 0]
             elif len(t_data.shape) == 3:
-                c_error = np.array([t_data[:, idx, 0] - m_data[:, idx]
+                c_error = np.array([m_data[:, idx] - t_data[:, idx, 0]
                                     for idx in range(m_data.shape[1])]).T
             else:
                 raise ValueError("Unknown Trajectory Format.")
             self._sim_data["results"].update(control_error=c_error)
 
         if "Observer" in self._sim_data["results"]:
-            o_error = (self._get_result_by_name("Solver")
-                       - self._get_result_by_name("Observer"))
+            o_error = (self._get_result_by_name("Observer")
+                       - self._get_result_by_name("Solver"))
             self._sim_data["results"].update(observer_error=o_error)
 
     def _get_result_by_name(self, name):