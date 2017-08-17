348042acab5e570f382bf139751c6607b8dc3919
Stefan Ecklebe <stefan.ecklebe@tu-dresden.de>
2017-08-17 14:03:28 +0200

    Removed illegal License classifier in package config

--- setup.py
+++ setup.py
@@ -29,7 +29,6 @@ setup(
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: GPLv3",
         "Natural Language :: English",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
