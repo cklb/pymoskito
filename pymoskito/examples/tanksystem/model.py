0500706099d3967858351184e0c91b583b0b647b
Jens Wurm <jens.wurm@umit.at>
2018-11-06 14:59:20 +0100

    Add example for cpp implementation, add pybind11 as lib, start implementation of cpp controller class

--- pymoskito/examples/tanksystem/model.py
+++ pymoskito/examples/tanksystem/model.py
@@ -7,45 +7,23 @@ from . import settings as st
 
 
 class TankModel(pm.Model):
-    """
-    Model of the 2 Tank System
-    """
-    public_settings = OrderedDict([('initial state', st.initial_state),
-                                   ('d_B', st.d_B),
-                                   ('d_R', st.d_R),
-                                   ('tube_length', st.tube_length),
-                                   ('k_L', st.k_L),
-                                   ('k_V', st.k_V),
-                                   ('k_s', st.k_s),
-                                   ('d', st.d),
-                                   ('T', st.T),
-                                   ('m', st.m),
-                                   ('g', st.g),
-                                   ])
+    public_settings = OrderedDict([('initial state', [0, 0]),
+                                   ("AT1", st.AT),
+                                   ("hT1", st.hT),
+                                   ("hV1", st.hV),
+                                   ("AS1", st.AS),
+                                   ("g", st.g),
+                                   ("Ku", st.Ku),
+                                   ("uA0", st.uA0)])
 
-    def __init__(self, settings):
+    def __init__(self, settings):  # Constructor
         # add specific "private" settings
-        settings.update(state_count=4)
+        settings.update(state_count=1)
         settings.update(input_count=1)
         settings.update({"output_info": {
-            0: {"Name": "ball elevation", "Unit": "m"},
+            0: {"Name": "Füllstand Tank", "Unit": "m"},
         }})
         pm.Model.__init__(self, settings)
-
-        # shortcuts for readability
-        self.d_B = self._settings['d_B']
-        self.d_R = self._settings['d_R']
-        self.tube_length = self._settings['tube_length']
-        self.k_L = self._settings['k_L']
-        self.k_V = self._settings['k_V']
-        self.k_s = self._settings['k_s']
-        self.d = self._settings["d"]
-        self.T = self._settings['T']
-        self.m = self._settings['m']
-        self.g = self._settings['g']
-        self.A_B = np.pi*self.d_B**2/4
-        self.A_R = np.pi*self.d_R**2/4
-        self.A_Sp = self.A_R - self.A_B
 
     def state_function(self, t, x, args):
         """
@@ -54,56 +32,47 @@ class TankModel(pm.Model):
         :param t: time
         :type args: system input u
         """
-
-        # definitional
         x1 = x[0]
         x2 = x[1]
-        # x3 = x[2] is not used
-        x4 = x[3]
-        u = args[0]
+        uA = args[0]
 
-        dx1 = x2
-        dx2 = -x1/self.T**2 - 2*self.d*x2/self.T + self.k_s*u*st.Vcc/(255*self.T**2)
-        dx3 = x4
-        dx4 = (self.k_L*((self.k_V*x1 - self.A_B*x4)/self.A_Sp)**2 - self.m*self.g)/self.m
+        uA0 = self.settings['uA0']
+        g = self.settings['g']
+        Ku = self.settings['Ku']
+        AS = self.settings['AS']
+        AT = self.settings['AT']
+        hV = self.settings['hV']
 
-        return np.array([dx1, dx2, dx3, dx4])
+        a1 = AS * np.sqrt(2 * g / (AT ** 2 - AS ** 2))
+
+        if uA < uA0:
+            u = np.array([0])
+        else:
+            u = uA - uA0
+
+        if x1 <= 0:
+            hV = 0
+
+        dx1 = - a1 * np.sign(x1 + hV) * np.sqrt(np.abs(x1 + hV)) + Ku / AT * u
+
+        return np.array([dx1])
 
     def root_function(self, x):
         """
-        in this case this means zero crossing detection for the balls elevation.
+        in this case this means zero crossing detection for the water height elevation.
         :param x: state
         """
         x0 = x
         flag = False
 
-        if x[2] <= 0:
-            x0[2] = 0
-            x0[3] = 0
-            flag = True
-
         if x[0] <= 0:
             x0[0] = 0
-            x0[1] = 0
             flag = True
 
         return flag, x0
 
-    def check_consistency(self, x):
-        """
-        Checks if the model rules are violated
-        :param x: state
-        """
-        if x[2] > (self._settings['tube_length']):  # + self._settings['tube_length']*0.2):
-            raise pm.ModelException('Ball flew out of the tube')
-
     def calc_output(self, input_vector):
-        """
-        return ball position as output
-        :param input_vector: input values
-        :return: ball position
-        """
-        return input_vector[2]
+        return [input_vector[0]]
 
 
 pm.register_simulation_module(pm.Model, TankModel)
