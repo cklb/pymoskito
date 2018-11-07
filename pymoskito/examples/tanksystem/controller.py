b6e612167adb45b0c32d714298789762347e4c1f
Jens <wurm.jens@gmail.com>
2018-11-07 01:23:33 +0100

    Update example for cpp pid implementation

--- pymoskito/examples/tanksystem/controller.py
+++ pymoskito/examples/tanksystem/controller.py
@@ -4,14 +4,21 @@ import numpy as np
 from collections import OrderedDict
 
 import pymoskito as pm
+from Controller import PIDController
 
 
 class CppPIDController(pm.CppController):
     """
-    manual pwm input
+    PID Controller implemented in cpp with pybind11
     """
-    public_settings = OrderedDict([("pwm", 160),
-                                   ("tick divider", 1)])
+    public_settings = OrderedDict([("Kp", 12),
+                                   ("Ti", 12),
+                                   ("Td", 12),
+                                   ("dt", 0.1),
+                                   ("output_limits", [0, 255]),
+                                   ("input_state", 1),
+                                   ("tick divider", 1),
+                                   ("path_cpp", 1)])
 
     def __init__(self, settings):
         # add specific "private" settings
@@ -19,15 +26,43 @@ class CppPIDController(pm.CppController):
         settings.update(output_dim=1)
         settings.update(input_type="system_state")
 
-        pm.Controller.__init__(self, settings)
+        pm.CppController.__init__(self, settings)
+
+        self.lastTime = 0
+        self.lastU = 0
+        self.pid = PIDController()
+        self.pid.create(self._settings["Kp"],
+                        self._settings["Ti"],
+                        self._settings["Td"],
+                        self._settings["output_limits"][0],
+                        self._settings["output_limits"][1],
+                        self._settings['dt'])
 
     def _control(self,
                  time,
                  trajectory_values=None,
                  feedforward_values=None,
-                 input_values=None, **kwargs):
+                 input_values=None,
+                 **kwargs):
+        # step size
+        dt = time - self.lastTime
 
-        u = self._settings["pwm"]
-        return np.array(u)
+        if np.isclose(dt, self._settings['dt']):
+            # save last control time
+            self.lastTime = time
+
+            # input abbreviations
+            x = input_values[int(self._settings['input_state'])]
+
+            yd = trajectory_values
+
+            u = np.array([self.pid.compute(x, yd)])
+        else:
+            u = self.lastU
+
+        self.lastU = u
+
+        return u
+
 
 pm.register_simulation_module(pm.Controller, CppPIDController)
