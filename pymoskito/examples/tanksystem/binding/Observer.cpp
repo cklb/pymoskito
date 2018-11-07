85450f6fb6dcfc2ff1c5cb6691c04a754230fbf8
Jens <wurm.jens@gmail.com>
2018-11-08 00:08:29 +0100

    Update test scenario to 2 tanks, add first approach for observer, Add write and test of CMakeLists.txt

---
+++ pymoskito/examples/tanksystem/binding/Observer.cpp
@@ -0,0 +1,80 @@
+/** @file Controller.cpp
+ * This file includes the different controller implementations for the ball in tube labor-imw test.
+ *
+ * Copyright (c) 2018 IACE
+ */
+#ifndef OBSERVER_CPP
+#define OBSERVER_CPP
+
+#include "Observer.h"
+
+
+void LuenbergerObserver::create(const double& dKp,
+                           const double& dTi,
+                           const double& dTd,
+                           const double& dOutputMin,
+                           const double& dOutputMax,
+                           const double& dSampleTime)
+{
+    this->dKp = dKp;
+    this->dTd = dTd;
+    this->dTi = dTi;
+	this->dOutputMin = dOutputMin;
+	this->dOutputMax = dOutputMax;
+
+	this->dSampleTime = dSampleTime;
+
+    this->dIntegral = 0.0;
+    this->dLastError = 0.0;
+}
+
+
+void LuenbergerObserver::reset()
+{
+    this->dIntegral = 0.0;
+    this->dLastError = 0.0;
+}
+
+
+double LuenbergerObserver::compute(const double& dCurInput,
+                              const double& dCurSetpoint)
+{
+	double dError = dCurSetpoint - dCurInput;
+
+	this->dIntegral += (dError + dLastError) * this->dSampleTime;
+
+	if (this->dIntegral > this->dOutputMax)
+		this->dIntegral = this->dOutputMax;
+	else if (this->dIntegral < this->dOutputMin)
+		this->dIntegral = this->dOutputMin;
+
+	// Compute differential part
+	double dDifferential = (dError - this->dLastError) / this->dSampleTime;
+
+	// Compute PID output
+    double dOut = 0.0;
+    if ((long long) (this->dTi * 1000) == 0LL)
+    {
+	    dOut = this->dKp * (dError + this->dTd * dDifferential);
+    }
+    else
+    {
+	    dOut = this->dKp * (dError +
+                            this->dIntegral / (2.0 * this->dTi) +
+                            this->dTd * dDifferential);
+    }
+
+    // Apply limit to output value
+    if (dOut > this->dOutputMax)
+        dOut = this->dOutputMax;
+    else if (dOut < this->dOutputMin)
+        dOut = this->dOutputMin;
+
+	// Keep track of some variables for next execution
+	this->dLastError = dError;
+
+	return dOut;
+}
+
+
+#endif // OBSERVER_CPP
