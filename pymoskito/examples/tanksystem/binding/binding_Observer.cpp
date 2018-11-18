a708e21ebc2d5311ac879559dcfc385679a2aa06
Jens <wurm.jens@gmail.com>
2018-11-18 23:15:39 +0100

    Add c++ version of observer for pymoskito with wrapper functions

--- pymoskito/examples/tanksystem/binding/binding_Observer.cpp
+++ pymoskito/examples/tanksystem/binding/binding_Observer.cpp
@@ -1,4 +1,5 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 #include "Observer.h"
 
 namespace py = pybind11;
@@ -7,8 +8,10 @@ PYBIND11_MODULE(Observer, m)
 {
     m.doc() = "Binding of a High Gain Observer";
 
-    py::class_<HighGainObserver>(m, "HighGainObserver")
+    py::class_<HighGainObserver> (m, "HighGainObserver")
     .def(py::init())
     .def("create", &HighGainObserver::create, "Create the observer with all needed constants and initialize the output vector")
-    .def("compute", &HighGainObserver::compute, "Calculates the observer output");
+    .def("compute", &HighGainObserver::compute, "Calculates the observer output")
+    .def("setGain", &HighGainObserver::setGain)
+    .def("setInitialState", &HighGainObserver::setInitialState);
 }
