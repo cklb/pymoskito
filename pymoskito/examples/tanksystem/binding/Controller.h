adc6ec74c4a16e1639b8a74291bc9d4c373e61ce
Stefan Ecklebe <stefan.ecklebe@tu-dresden.de>
2018-11-27 16:08:43 +0100

    Some thoughts on the bindings: * Refactored the src files to contain one class per file * Renamed the Controller class to fit the name of its binding   (Observer is still to be done) * Moved the binding generation out of the registration process.   The registry module should not have anything to do with something else   than registration. * The introduced extra property `Module` not quite meaningful and should   not be the way to do things. The whole process should not depend on   whether a pure python or a wrapper module is used. * Refactored the binding generation in the `tools` module to get it pep8   conform and to remove code duplicates. * As it seems to me atm an installation of `pybind11` via pip misses the   required `.cmake` files for the `find_package(pybind11)` call to   succeed. I'm opening an issue upstream to get further information on   this topic. * After a little though on the topic, I really dislike the variant on   using pybind as a git submodule since it greatly complicates the   installation process of pymoskito for an optional dependency. * Added a pybind entry to the requirements

--- pymoskito/examples/tanksystem/binding/Controller.h
+++ pymoskito/examples/tanksystem/binding/Controller.h
@@ -1,5 +1,5 @@
 /** @file Controller.h
- * This file includes the different controller implementations for the two tank system.
+ * This file includes the base controller implementation for the two tank system.
  *
  */
 #ifndef CONTROLLER_H
@@ -80,48 +80,6 @@ protected:
     double dOutputMin = -255.0;     ///< Mininal value for the calculated output
     double dOutputMax = 255.0;      ///< Maximal value for the calculated output
     double dSampleTime = 0.0;       ///< Sample time of the controller
-};
-
-/**
- * @brief Class that is derived from the Controller class and implements a PID controller.
- */
-class PIDController : public Controller {
-private:
-    double dIntegral = 0;   ///< Value of the integral part
-    double dLastError = 0;  ///< Value of error from the last time step
-
-public:
-    /// Constructor of the PID controller
-    PIDController() {}
-
-    /// Destructor of the PID controller
-    ~PIDController() {}
-
-    void create(const double &dKp,
-                const double &dTi,
-                const double &dTd,
-                const double &dOutputMin,
-                const double &dOutputMax,
-                const double &dSampleTime);
-
-    void reset();
-
-    double compute(const double &dCurInput,
-                   const double &dCurSetpoint);
-
-    /**
-     * @brief Method that returns the current integral part of the controller
-     *
-     * @return integral part
-     */
-    double getIntegral() const { return dIntegral; }
-
-    /**
-     * @brief Method that returns the error from the last time step
-     *
-     * @return error from the last time step
-     */
-    double getLastError() const { return dLastError; }
 };
 
 #endif // CONTROLLER_H
