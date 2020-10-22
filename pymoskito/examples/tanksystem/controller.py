4e6d698732c2adb02590a82fc475712a93bc641f
Jens Wurm <jens.wurm@umit.at>
2020-10-22 11:24:15 +0200

    Rename folder for better understanding

--- pymoskito/examples/tanksystem/controller.py
+++ pymoskito/examples/tanksystem/controller.py
@@ -52,9 +52,9 @@ class CppPIDController(pm.Controller, pm.CppBase):
 
         m_path = os.path.dirname(__file__)
         if 'tanksystem' not in m_path:
-            m_path += '/pymoskito/examples/tanksystem/utils/'
+            m_path += '/pymoskito/examples/tanksystem/src/'
         else:
-            m_path += '/utils/'
+            m_path += '/src/'
 
         pm.Controller.__init__(self, settings)
         pm.CppBase.__init__(self,
