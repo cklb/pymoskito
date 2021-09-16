0f0d0b075514266784cc7e6ef43bfb050686e281
Stefan Ecklebe <stefan.ecklebe@tu-dresden.de>
2021-09-16 16:25:37 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ extra_requirements = {
 
 setup(
     name="pymoskito",
-    version="0.4.0rc3",
+    version="0.4.0rc4",
     description="Python based modular simulation & postprocessing kickass "
                 "toolbox",
     long_description=readme + "\n\n" + history,
